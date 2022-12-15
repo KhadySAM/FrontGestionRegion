9b21964f7b89cf5e42720a23e1b5263ccbe3b856
Kadi <ksamake18@gmail.com>
2022-12-15 11:10:57 +0000

    pages ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './Pages/Accueil/accueil/accueil.component';
+import { LieuxComponent } from './Pages/Lieuxtouristique/lieux/lieux.component';
+import { RegionComponent } from './Pages/Region/region/region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'accueil',
+    component: AccueilComponent
+  },
+  {
+    path:'region',
+    component: RegionComponent
+  },
+  {
+    path:'lieux',
+    component: LieuxComponent
+  },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+ 
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './Pages/Accueil/accueil/accueil.component';
+import { RegionComponent } from './Pages/Region/region/region.component';
+import { LieuxComponent } from './Pages/Lieuxtouristique/lieux/lieux.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    RegionComponent,
+    LieuxComponent
   ],
   imports: [
     BrowserModule,
